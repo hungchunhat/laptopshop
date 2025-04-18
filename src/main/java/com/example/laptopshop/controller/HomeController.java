4068651c439b8bdd303b0ced10acad83a25e5de0
hungchunhat <maxtroll2001@gmail.com>
2025-04-18 19:19:41 +0700

    2nd commit

--- src/main/java/com/example/laptopshop/controller/HomeController.java
+++ src/main/java/com/example/laptopshop/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.example.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
-    @RequestMapping("home")
+    @GetMapping("/")
     public String home() {
-        return "index";
+        return "<h1>Hello World from Hưng đẹp trai</h1>";
     }
 }
