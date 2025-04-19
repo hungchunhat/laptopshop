1dad59afdf320c189bfccc3d38b4415c017a3b47
hungchunhat <maxtroll2001@gmail.com>
2025-04-19 15:42:52 +0700

    3rd commit

--- src/main/java/com/example/laptopshop/controller/HomeController.java
+++ src/main/java/com/example/laptopshop/controller/HomeController.java
@@ -9,6 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "<h1>Hello World from Hưng đẹp trai</h1>";
+        return "<h1>Hello World from home</h1>";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "<h1>Only user can access this page</h1>";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "<h1>Only admin can access this page</h1>";
     }
 }
